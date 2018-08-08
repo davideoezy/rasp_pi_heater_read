ce93de4d9d4624d16b7f6c7517b75327e8cad687
David Erving <david.erving@hotmail.com>
2018-08-08 19:47:56 +1000

    Complete reader script

--- temp_reader.py
+++ temp_reader.py
@@ -2,11 +2,29 @@
 import mysql.connector as mariadb
 import glob
 import sys
+import time
+
+# ---------------- Initialise variables ------------------
+# Device id
+device = "RPi_1"
+# Server db connection
+host_ip = '192.168.0.10'
+host_port = '3308'
+host_db_name = 'pi_test'
+host_uid = 'pi'
+host_pword = 'raspberry'
+
 
 #Conect to sensor
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+# Read device info
+def read_rom():
+    name_file=device_folder+'/name'
+    f = open(name_file,'r')
+    return f.readline()
 
 #Raw temp
 def read_temp_raw():
@@ -24,20 +42,16 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        #Not even the best solution, but seems the DS18B20 reads 3* upper than the real
-        return temp_c - 3
-
-device = "RPi_1"
+        return temp_c
 
 #Connect to mariadb
-con = mariadb.connect(host='192.168.0.10', port='3306', user='pi', password='raspberry', database='pi_test')
-cur = con.cursor()
 
-try:
-        cur.execute("""INSERT INTO temperature (device,temp)
-                    VALUES ('{}',{})""".format(test_string,read_temp()))
+while True:
+    con = mariadb.connect(host=host_ip, port=host_port, user=host_uid, password=host_pword, database=host_db_name)
+    cur = con.cursor()
+    try:
+        cur.execute("""INSERT INTO temperature (device,temp) VALUES ('{}',{})""".format(device,read_temp()))
         con.commit()
-except:
+    except:
         con.rollback()
-
-con.close()
+    con.close()
